c5612d01e9b2f8ef69b3c68ec57a24b27b4c9596
aalexaashkaa <157103630+aalexaashkaa@users.noreply.github.com>
2026-03-24 22:50:38 +0400

    Add logging

--- msdt-4/leaf_in_tree.py
+++ msdt-4/leaf_in_tree.py
@@ -1,5 +1,16 @@
 import sys
+import logging
 import os
+
+os.makedirs('logs', exist_ok=True)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.FileHandler(os.path.join('logs', "l4_logs.log"), encoding="utf-8")]
+)
+logger = logging.getLogger("l4_logs")
 
 
 class Node:
@@ -68,13 +79,16 @@ def parsing(f):
     """Парсинг файла"""
 
     tree = Tree()
+    logger.info(f"Start parsing file: {f.name}")
 
     for line_num, line in enumerate(f, 1):
     
         if ':' not in line:
+            logger.error(f"Line {line_num}: Missing ':' symbol")
             raise ValueError("Строка не содержит символ ':'")
         
         if ' ' in line:
+            logger.error(f"Line {line_num}: Contains spaces")
             raise ValueError("Строка содержит пробелы")
         
         st = line.strip().split(':')
@@ -82,11 +96,13 @@ def parsing(f):
         tail = st[1].split(',')
 
         if not head:
+            logger.error(f"Line {line_num}: Empty node name before ':'")
             raise ValueError("Пустое имя узла перед символом ':'")
 
         if tree.get_node(head) is None:
             new_node = Node(head)
             tree.nodes[head] = new_node
+            logger.debug(f"Node created in tree: '{new_node.value}'")
     
         else:
             new_node = tree.get_node(head)
@@ -97,12 +113,14 @@ def parsing(f):
                 if tree.get_node(i) == None:
                     child_node = Node(i)
                     tree.nodes[i] = child_node
+                    logger.debug(f"Node created in tree: '{child_node.value}'")
                 else:
                     child_node = tree.get_node(i)
 
                 new_node.add_child(child_node)
 
         if len(new_node.children) != len(set(new_node.children)):
+            logger.error(f"Line {line_num}: Non-unique name")
             raise ValueError("Присутствие узлов с неуникальными названиями")
     
     #После парсинга определяется корень дерева
@@ -110,15 +128,19 @@ def parsing(f):
         parents = tree.get_parent(node)
 
         if len(parents) > 1:
+            logger.error(f"Node '{node.value}' has multiple parents")
             raise ValueError("Присутствие узлов с двумя или более родителями")
 
         elif len(parents) == 0 and tree.root == None:
             tree.root = node
+            logger.info(f"Root node identified: '{tree.root.value}'") 
 
         elif len(parents) == 0 and tree.root != None:
+            logger.error(f"Multiple root nodes: {tree.root.value} and {node.value}")
             raise ValueError("Присутствие более одного корневого узла")
         
     if tree.root == None:
+        logger.error(f"No root node")
         raise ValueError("Отсутствие корневого узла")
     return tree
 
@@ -126,19 +148,30 @@ def parsing(f):
 def run(file_name):
     """Запуск обработки файла"""
 
+    logger.info(f"Attempting to open file: {file_name}")
+
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
+            logger.info(f"File opened successfully: {file_name}")
+
             new_tree = parsing(file)
+            logger.debug(f"File content parsed successfully: {file_name}")
+
             leaf_count = new_tree.get_count_leafs()
             print(f'Количество листьев в данном дереве: {leaf_count}')
+            logger.info(f"Leaf count:  '{leaf_count}'")
+
+            logger.info(f"Closing file: {file_name}")
             return leaf_count
         
     except FileNotFoundError:
         print(f"Файл {file_name} не найден")
+        logger.error(f"File not found: {e}")
         raise
 
     except ValueError as e:
         print(f"Ошибка в формате данных: {e}", file=sys.stderr)
+        logger.error(f"Validation error: {e}")
         raise
 
 if __name__ == '__main__':
